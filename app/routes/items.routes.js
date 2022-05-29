5579d0529732be95a345fa49cd41fc655eaa3323
koirul92 <koirulanwar98@gmail.com>
2022-05-29 10:06:26 +0700

    fix

--- app/routes/items.routes.js
+++ app/routes/items.routes.js
@@ -2,7 +2,7 @@ module.exports = (app) => {
     const items = require('../controllers/items.controller')
     const router = require('express').Router()
 
-    //router.get('/', users.list)
+    router.get('/', items.list)
     router.post('/', items.create)
 
     app.use('/api/items', router)
