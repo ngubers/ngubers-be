186758c003e3ad8a7993e1082794461720cb860b
Milda Aditia Putra <aditia.putra100@gmail.com>
2022-06-12 19:52:06 +0700

    fix: add API KEY for customer

---
+++ app/models/order.model.js
@@ -0,0 +1,52 @@
+module.exports = (mongoose) => {
+    const schema = mongoose.Schema(
+        {
+            userId: {
+                type: String,
+                required: true
+            },
+            service:{
+                type: String,
+                required: true
+            },
+            from:{
+                type: String,
+                required: true
+            },
+            destination:{
+                type: String,
+                required: true
+            },
+            driverId:{
+                type: ObjectId,
+                required: true
+            },
+            price:{
+                type: String,
+                required: true
+            },
+            date:{
+                type: String,
+                required: true
+            },
+            description:{
+                type: String,
+                required: true
+            },
+            status:{
+                type: String,
+                required: true
+            }
+        },
+        {timestamps: true}
+    )
+
+    schema.method("toJson", function() {
+        const {__v, _id, ...object} = this.toObject()
+        object.id = _id
+        return object
+    })
+
+    const Order = mongoose.model("orders", schema)
+    return Order
+}
---
+++ app/services/order.service.js
@@ -0,0 +1,56 @@
+const userRepository = require('../repositories/user.respository')
+const orderRepository = require('../repositories/order.respository')
+
+module.exports = {
+    async list() {
+        try {
+            // const user = await userRepository.findAll()
+            const order = await orderRepository.findAll()
+
+            return {
+                data: order
+            }
+        }
+        catch(error) {
+            console.log(error)
+        }
+    },
+
+    async create(args) {
+        try {
+            const user = await userRepository.create(args)
+            return user.save()
+        }
+        catch(error) {
+            console.log(error)
+        }
+    },
+
+    async find(id) {
+        try {
+            const user = await userRepository.findById(id)
+            return user
+        } catch(error) {
+            console.log(error)
+        }
+    },
+
+    async findByEmail(email) {
+        try {
+            const user = await userRepository.findByEmail(email)
+            return user
+        }
+        catch(error) {
+            console.log(error)
+        }
+    },
+
+    async update(id, args) {
+        try {
+            const user = await userRepository.update(id, args)
+            return user
+        } catch(error) {
+            console.log(error)
+        }
+    }
+}
